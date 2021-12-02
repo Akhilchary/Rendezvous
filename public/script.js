fa7ac089a0723b5c9bd92c668961f0dfda5f9c3f
noobguy77 <impmailonly7@gmail.com>
2021-12-02 18:33:43 +0530

    own video and guest video implemented

--- public/script.js
+++ public/script.js
@@ -1,6 +1,5 @@
-//frontend js
-//used for showing own video
-
+//client side js
+var socket = io();
 //this is the video grid element in HTML
 const videoGrid = document.getElementById('video-grid');
 
@@ -8,6 +7,14 @@ const videoGrid = document.getElementById('video-grid');
 const myVideo = document.createElement('video');
 console.log(myVideo);
 myVideo.muted = true;
+
+//here we want to run our own peer server
+
+var peer = new Peer(undefined, {
+    path: '/peerjs',
+    host: '/',
+    port: '6969'
+});
 
 //this is used to ger perm from user to access media like video and audio
 //it returns a promise, if user rejects we can handle that case differently
@@ -21,8 +28,49 @@ navigator.mediaDevices.getUserMedia({
     myVideoStream = stream;
     //addVideoStream function adds stream to myVideo
     addVideoStream(myVideo, stream);
+    //answering to other users call
+    peer.on('call', call => {
+        call.answer(stream);
+        // const video = document.createElement('video');
+        call.on('stream', userVideoStream => {
+            addVideoStream(myVideo, userVideoStream);
+        });
+    });
+
+
+    socket.on('user-connected', (userID) => {
+        connectToNewUser(userID, stream);
+    });
+});
+
+
+//open event is emitted when a connection is established to peer server
+peer.on('open', id => {
+    console.log(id);
+    socket.emit('join-room', ROOM_ID, id);
 })
 
+//we are emitting an event join-room signifing that user has joined
+// socket.emit('join-room', ROOM_ID);
+
+//catching user-connected event here
+// socket.on('user-connected', (userID) => {
+//     connectToNewUser(userID,stream);
+// });
+
+const connectToNewUser = (userID, stream) => {
+    console.log('new user bitchh and id is', userID);
+
+    //exisiting user is calling to the newly joined user
+    const call = peer.call(userID, stream);
+
+    //creating a video element for 
+    // const video = document.createElement('video');
+    call.on('stream', userVideoStream => {
+        addVideoStream(myVideo, userVideoStream);
+    })
+
+}
 
 const addVideoStream = (video, stream) => {
     //The srcObject property of the HTMLMediaElement interface sets or returns the object which serves as the source of the media associated with the HTMLMediaElement.
